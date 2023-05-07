236886f14e0a43973e3381fd884b493b189a9c5d
JLPuente <puenteformacio@gmail.com>
2023-05-07 19:27:47 +0200

    sis

--- src/components/SocialList.tsx
+++ src/components/SocialList.tsx
@@ -7,14 +7,29 @@ export function SocialList({}) {
   return (
     <div>
       <a
-        title="Linkedin"
-        href={`https://www.linkedin.com/in/puentedesign/${config.twitter_account}`}
+        title="Twitter"
+        href={`https://twitter.com/${config.twitter_account}`}
         target="_blank"
         rel="noopener"
       >
         <Twitter width={24} height={24} fill={"#222"} />
       </a>
-  
+      <a
+        title="GitHub"
+        href={`https://github.com/${config.github_account}`}
+        target="_blank"
+        rel="noopener"
+      >
+        <GitHub width={24} height={24} fill={"#222"} />
+      </a>
+      <a
+        title="Linkedin"
+        href={`https://linkedin.com/${config.github_account}`}
+        target="_blank"
+        rel="noopener"
+      >
+        <Linkedin width={24} height={24} fill={"#222"} />
+      </a>
       <style jsx>{`
         a {
           display: inline-block;
